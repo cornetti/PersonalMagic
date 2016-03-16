1ba07c9891f902316b14169af840f2f565ebe5ef
kotochan <kotono.yoshida@gmail.com>
2016-03-16 18:45:42 +0100

    Classe FalsePeace creatah

--- src/cardgame/cards/FalsePeace.java
+++ src/cardgame/cards/FalsePeace.java
@@ -1,7 +1,48 @@
 package cardgame.cards;
 
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+
 /**
  * Created by Kotono on 16/03/2016.
  */
-public class FalsePeace {
+public class FalsePeace implements Card {
+
+    private class FalsePeaceEffect extends AbstractCardEffect {
+        public FalsePeaceEffect(Player p, Card c){
+            super(p,c);
+        }
+
+        @Override
+        public void resolve() {
+
+        }
+    }
+
+    @Override
+    public Effect get_effect(Player owner) {
+        return new FalsePeaceEffect(owner,this);
+    }
+
+    @Override
+    public String name() {
+        return "False Peace";
+    }
+
+    @Override
+    public String type() {
+        return "Sorcery";
+    }
+
+    @Override
+    public String rule_text() {
+        return "Target player skips his next combat phase";
+    }
+
+    @Override
+    public boolean isInstant() {
+        return false;
+    }
 }

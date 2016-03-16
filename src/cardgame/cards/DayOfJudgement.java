e1fe36dc6d264f5d5a31d1f563cae9fececfca9b
kotochan <kotono.yoshida@gmail.com>
2016-03-16 18:43:19 +0100

    Classe DayOfJudgement creatah

--- src/cardgame/cards/DayOfJudgement.java
+++ src/cardgame/cards/DayOfJudgement.java
@@ -1,7 +1,48 @@
 package cardgame.cards;
 
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+
 /**
  * Created by Kotono on 16/03/2016.
  */
-public class DayOfJudgement {
+public class DayOfJudgement implements Card {
+
+    private class DayOfJudgementEffect extends AbstractCardEffect {
+        public DayOfJudgementEffect(Player p, Card c){
+            super(p,c);
+        }
+
+        @Override
+        public void resolve() {
+
+        }
+    }
+
+    @Override
+    public Effect get_effect(Player owner) {
+        return new DayOfJudgementEffect(owner,this);
+    }
+
+    @Override
+    public String name() {
+        return "Day of Judgement";
+    }
+
+    @Override
+    public String type() {
+        return "Sorcery";
+    }
+
+    @Override
+    public String rule_text() {
+        return "Destroy all creatures";
+    }
+
+    @Override
+    public boolean isInstant() {
+        return false;
+    }
 }

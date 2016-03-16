67b0a5a4522bbb08a60ef474c47786ad856ab754
kotochan <kotono.yoshida@gmail.com>
2016-03-16 18:50:32 +0100

    Classe Fatigue creatah

--- src/cardgame/cards/Fatigue.java
+++ src/cardgame/cards/Fatigue.java
@@ -1,7 +1,48 @@
 package cardgame.cards;
 
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+
 /**
  * Created by Kotono on 16/03/2016.
  */
-public class Fatigue {
+public class Fatigue implements Card {
+
+    private class FatigueEffect extends AbstractCardEffect {
+        public FatigueEffect(Player p, Card c){
+            super(p,c);
+        }
+
+        @Override
+        public void resolve() {
+
+        }
+    }
+
+    @Override
+    public Effect get_effect(Player owner) {
+        return new FatigueEffect(owner,this);
+    }
+
+    @Override
+    public String name() {
+        return "Fatigue";
+    }
+
+    @Override
+    public String type() {
+        return "Sorcery";
+    }
+
+    @Override
+    public String rule_text() {
+        return "Target player skips his or her next draw step";
+    }
+
+    @Override
+    public boolean isInstant() {
+        return false;
+    }
 }

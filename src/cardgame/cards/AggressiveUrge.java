b71693a093a865e68b4b213b1564815aa21d9e68
kotochan <kotono.yoshida@gmail.com>
2016-03-16 19:00:54 +0100

    Classe AggressiveUrge creatah

--- src/cardgame/cards/AggressiveUrge.java
+++ src/cardgame/cards/AggressiveUrge.java
@@ -1,7 +1,49 @@
 package cardgame.cards;
 
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+
 /**
  * Created by Kotono on 16/03/2016.
  */
-public class AggressiveUrge {
+public class AggressiveUrge implements Card {
+
+    private class AggressiveUrgeEffect extends AbstractCardEffect {
+        public AggressiveUrgeEffect(Player p, Card c){
+            super(p,c);
+        }
+
+        @Override
+        public void resolve() {
+
+        }
+    }
+
+
+    @Override
+    public Effect get_effect(Player owner) {
+        return new AggressiveUrgeEffect(owner,this);
+    }
+
+    @Override
+    public String name() {
+        return "Aggressive Urge";
+    }
+
+    @Override
+    public String type() {
+        return "Instant";
+    }
+
+    @Override
+    public String rule_text() {
+        return "Target creature gets +1/+1 until end of turn";
+    }
+
+    @Override
+    public boolean isInstant() {
+        return true;
+    }
 }

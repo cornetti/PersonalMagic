bd11a25b9f29f31c3ff6005b2333a0a752ca4287
kotochan <kotono.yoshida@gmail.com>
2016-03-16 18:52:50 +0100

    Classe Afflict creatah

--- src/cardgame/cards/Afflict.java
+++ src/cardgame/cards/Afflict.java
@@ -1,7 +1,48 @@
 package cardgame.cards;
 
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+
 /**
  * Created by Kotono on 16/03/2016.
  */
-public class Afflict {
+public class Afflict implements Card {
+
+    private class AfflictEffect extends AbstractCardEffect {
+        public AfflictEffect(Player p, Card c){
+            super(p,c);
+        }
+
+        @Override
+        public void resolve() {
+
+        }
+    }
+
+    @Override
+    public Effect get_effect(Player owner) {
+        return new AfflictEffect(owner,this);
+    }
+
+    @Override
+    public String name() {
+        return "Afflict";
+    }
+
+    @Override
+    public String type() {
+        return "Instant";
+    }
+
+    @Override
+    public String rule_text() {
+        return "Target creature gets -1/-1 until end of turn";
+    }
+
+    @Override
+    public boolean isInstant() {
+        return true;
+    }
 }

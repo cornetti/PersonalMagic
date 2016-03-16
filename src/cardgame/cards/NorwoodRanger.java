0631e866d9e3af7fc46a1708a3b13d4a859d2c1d
kotochan <kotono.yoshida@gmail.com>
2016-03-16 19:16:46 +0100

    Classe NorwoodRanger creatah

--- src/cardgame/cards/NorwoodRanger.java
+++ src/cardgame/cards/NorwoodRanger.java
@@ -1,7 +1,48 @@
 package cardgame.cards;
 
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+
 /**
  * Created by Kotono on 16/03/2016.
  */
-public class NorwoodRanger {
+public class NorwoodRanger implements Card {
+
+    private class NorwoodRangerEffect extends AbstractCardEffect {
+        public NorwoodRangerEffect(Player p, Card c){
+            super(p,c);
+        }
+
+        @Override
+        public void resolve() {
+
+        }
+    }
+
+    @Override
+    public Effect get_effect(Player owner) {
+        return new NorwoodRangerEffect(owner,this);
+    }
+
+    @Override
+    public String name() {
+        return "Norwood Ranger";
+    }
+
+    @Override
+    public String type() {
+        return "Creature";
+    }
+
+    @Override
+    public String rule_text() {
+        return "The song of the forest is in perfect harmony. If a single note is out of place, the elves will find its source";
+    }
+
+    @Override
+    public boolean isInstant() {
+        return false;
+    }
 }
--- src/cardgame/cards/VolcanicHammer.java
+++ src/cardgame/cards/VolcanicHammer.java
@@ -1,7 +1,48 @@
 package cardgame.cards;
 
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+
 /**
  * Created by Kotono on 16/03/2016.
  */
-public class VolcanicHammer {
+public class VolcanicHammer implements Card {
+
+    private class CancelEffect extends AbstractCardEffect {
+        public CancelEffect(Player p, Card c){
+            super(p,c);
+        }
+
+        @Override
+        public void resolve() {
+
+        }
+    }
+
+    @Override
+    public Effect get_effect(Player owner) {
+        return null;
+    }
+
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public String type() {
+        return null;
+    }
+
+    @Override
+    public String rule_text() {
+        return null;
+    }
+
+    @Override
+    public boolean isInstant() {
+        return false;
+    }
 }

f0fd079d968a13f7febd480a536134f9c0fa7e6b
kotochan <kotono.yoshida@gmail.com>
2016-03-16 19:06:17 +0100

    Classe BoilingEarth creatah

--- src/cardgame/cards/BoilingEarth.java
+++ src/cardgame/cards/BoilingEarth.java
@@ -1,7 +1,48 @@
 package cardgame.cards;
 
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+
 /**
  * Created by Kotono on 16/03/2016.
  */
-public class BoilingEarth {
+public class BoilingEarth implements Card {
+
+    private class BoilingEarthEffect extends AbstractCardEffect {
+        public BoilingEarthEffect(Player p, Card c){
+            super(p,c);
+        }
+
+        @Override
+        public void resolve() {
+
+        }
+    }
+
+    @Override
+    public Effect get_effect(Player owner) {
+        return new BoilingEarthEffect(owner,this);
+    }
+
+    @Override
+    public String name() {
+        return "Boiling Earth";
+    }
+
+    @Override
+    public String type() {
+        return "Sorcery";
+    }
+
+    @Override
+    public String rule_text() {
+        return "Boiling Earth deals 1 damage to each creature";
+    }
+
+    @Override
+    public boolean isInstant() {
+        return false;
+    }
 }

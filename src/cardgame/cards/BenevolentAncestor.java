a96dd10c76c8d7038dcaa6d782987a38e7b92d52
kotochan <kotono.yoshida@gmail.com>
2016-03-24 18:06:12 +0100

    Update BenevolentAncestor.java

--- src/cardgame/cards/BenevolentAncestor.java
+++ src/cardgame/cards/BenevolentAncestor.java
@@ -14,16 +14,42 @@ public class BenevolentAncestor implements Card {
         public BenevolentAncestorEffect(Player p, Card c){
             super(p,c);
         }
-
-        @Override
-        public void resolve() {
-
-        }
     }
 
     @Override
     public Effect get_effect(Player owner) {
         return new BenevolentAncestorEffect(owner,this);
+    }
+
+       private class BenevolentAncestorCreature extends AbstractCreature {
+        ArrayList<Effect> all_effects= new ArrayList<>();
+        ArrayList<Effect> tap_effects= new ArrayList<>();
+
+        BenevolentAncestorCreature(Player owner) { /*Costruttore*/
+            super(owner);
+            all_effects.add( new Effect() {
+                                 public boolean play(ArrayList<? extends Target> targets) {
+                                     CardGame.instance.get_stack().add(this);
+                                     return tap();
+                                 }
+
+                                 public void resolve() {}
+
+                                 public String toString()
+                                 { return "Benevolent Ancestor"; } /*commento dummy*/
+                             }
+            );
+        }
+
+        public String name() { return "Benevolent Ancestor"; }
+
+        public void attack() {}
+        public void defend(Creature c) {}
+        public int get_power() { return 0; }
+        public int get_toughness() { return 4; }
+
+        public List<Effect> effects() { return all_effects; }
+        public List<Effect> avaliable_effects() { return (is_tapped)?tap_effects:all_effects; }
     }
 
     @Override
@@ -46,3 +72,4 @@ public class BenevolentAncestor implements Card {
         return false;
     }
 }
+

b65705d31897ad2a762102cc04b9893c4951d597
squaeodiogha <giacoandre94@gmal.com>
2016-04-09 00:23:45 +0200

    testata darkness ed è finita

--- src/cardgame/cards/Darkness.java
+++ src/cardgame/cards/Darkness.java
@@ -2,9 +2,7 @@ package cardgame.cards;
 
 import cardgame.*;
 
-/**
- * Created by Kotono on 16/03/2016.
- */
+//works
 public class Darkness implements Card {
 
     private class DarknessEffect extends AbstractCardEffect {
